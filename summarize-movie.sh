2362765d9ebb410c5d7bfeea80747ac9d50e86ab
vackosar <vackosar@x.x>
2016-03-25 13:18:11 +0100

    Sanitize

--- summarize-movie.sh
+++ summarize-movie.sh
@@ -1,19 +1,19 @@
 getStart () {
-	grep -B1 "${match}" "tmp/subtitles.txt" |
+	grep -B1 "${match//\./\\.}" "tmp/subtitles.txt" |
 		grep '[0-9][0-9]:[0-9][0-9]' | tr ',' '.' |awk '{print $1}'; 
 }
 getEnd () {
-	grep -B1 "${match}" "tmp/subtitles.txt" |
+	grep -B1 "${match//\./\\.}" "tmp/subtitles.txt" |
 		grep '[0-9][0-9]:[0-9][0-9]' | tr ',' '.' |awk '{print $3}';
 }
 addSeconds() {
 	echo "$(date --date "@$(($(date --date "${1}" +%s)+${2}))" +%H:%M:%S)";
 }
 matchStart () {
-	end=$((lenght-1));
+	end=$lenght;
         while ! [ ${end} = ${position} ]; do
                 match="${line:position:end}";
-                if grep -q "${match}" "tmp/subtitles.txt"; then
+                if grep -q "${match//\./\\.}" "tmp/subtitles.txt"; then
                         addSeconds "$(getStart)" "-1";
                         return;
                 fi;
@@ -23,15 +23,15 @@ matchStart () {
 }
 matchEnd () {
 	matchStart > /dev/null;
-	if [ ${end} = $((lenght-1)) ]; then 
+	if [ ${end} = ${lenght} ]; then 
 		addSeconds "$(getEnd)" "3";
 		return;
 	fi
 	position=$((end+1));
-	end=$((lenght-1));
+	end=${lenght};
         while ! [ ${end} = ${position} ]; do
                 match="${line:position:end}";
-                if grep -q "${match}" "tmp/subtitles.txt"; then
+                if grep -q "${match//\./\\.}" "tmp/subtitles.txt"; then
                         stamp="$(getEnd)";
 			echo "$(date --date "@$(($(date --date "$stamp" +%s)+3))" +%H:%M:%S)";
                         return;
@@ -46,12 +46,13 @@ match () {
 	position=0;
 	start="$(matchStart)";
 	finish="$(matchEnd)";
-	echo "${start} ${finish} ${line}";
+	echo "${lineId} ${start} ${finish} ${line}" >> tmp/extracts.txt;
 	cat /dev/null | ffmpeg -nostats -loglevel panic -i "$movie" -ss "${start}" -to "${finish}" -c copy tmp/output${lineId}.mp4;
 	lineId=$((lineId+1));
 }
 sanitizeSubtitles () {
 	cat "$subtitles" |
+		sed 's/\.\+/\./g' |
 		sed 's/([^)]*)//g' |
 		sed 's/<[^>]*>//g' |
                 tr '-' '_' | tr '(' '_' | tr ')' '_' | tr -d '\r' | grep -v '^[0-9]*$' |
